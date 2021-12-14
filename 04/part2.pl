dcce564eddc5117c22c74eca213cb0273bf3477e
Steve Kay <stevekay@gmail.com>
2021-12-14 22:46:19 +0000

    day4 part2

--- 04/part2.pl
+++ 04/part2.pl
@@ -11,7 +11,9 @@ while(<>) {
   push @{$card[$c]},$1;
  } 
 }
-
+for $a ( 0 .. $#c ) {
+ @win[$a]=0;
+}
 for(@num) {
  $drawn{$_}='*';
 
@@ -26,11 +28,17 @@ for(@num) {
    $row=int($b/5);
    if($drawn{$v} eq '*') { $rowtot++ }
    if($rowtot==5) { 
+    $tot=0;
     for $z ( 0 .. 24 ) {
      $v=$card[$a][$z];
      if($drawn{$v} ne '*') { $tot+=$v }
     }
-    die $tot * $_;
+    $win[$a]=1;
+    $p=0;
+    for $pp ( 0 .. $#card ) {
+     if($win[$pp]) { $p++ }
+    }
+    if($p == $#card + 1) { die $_ * $tot }
    }
 
    # check for winning column
@@ -38,11 +46,17 @@ for(@num) {
   }
   for $zz ( 0 .. 4 ) {
    if($coltots[$zz] == 5) {
+    $tot=0;
     for $z ( 0 .. 24 ) {
      $v=$card[$a][$z];
      if($drawn{$v} ne '*') { $tot+=$v }
     }
-    die $tot * $_;
+    $win[$a]=1;
+    $p=0;
+    for $pp ( 0 .. $#card ) {
+     if($win[$pp]) { $p++ }
+    }
+    if($p == $#card + 1) { die $_ * $tot }
    }
   }
  }
